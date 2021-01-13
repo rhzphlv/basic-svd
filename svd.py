fddaa42c554e946728e5dc03e83fd4519f55c4aa
rhzphlv <72447266+rhzphlv@users.noreply.github.com>
2021-01-13 16:40:03 +0700

    Update svd.py

--- svd.py
+++ svd.py
@@ -45,3 +45,27 @@ dokumen=np.dot(sigma,vh)
 #transpose matrix dokumen agar dapat menghasilkan dikalikan(dot product)
 #dengan algoritma_pemrograman
 dokumen=dokumen.transpose()
+
+#Perhitungan jarak antara dokumen dan kata
+#menggunakan jarak cosinus
+
+#menghitung perkalian (dot) dokumen dan pemrograman
+dot_dokumen_algoritma=np.dot(d,algoritma_pemrograman)
+
+#menghitung Panjang tiap vector dokumen 
+len_dokumen=lg.norm(dokumen,axis=1)
+
+#menghitung panjan vector algoritma pemrograman
+len_algoritma_pemrograman=lg.norm(algoritma_pemrograman)
+
+
+jarak_dokumen=[]
+for i in range(len(len_dokumen)):
+	#untuk menghindari nilai Nan
+    if dot_dokumen_algoritma [i]>0.00001:
+        jarak= dot_dokumen_algoritma[i]/
+  (len_dokumen[i]*len_ algoritma_pemrograman)   
+    else:
+        jarak=0 
+    jarak_dokumen.append(jarak)
+
